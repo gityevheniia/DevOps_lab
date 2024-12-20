b4569f99aed2186b18deb8ce99bab4954c03ef1d
yevheniia <yevheniia@UBUNTU01.myguest.virtualbox.org>
2024-12-20 22:39:18 +0200

    Solved conflict

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,11 +1,9 @@
-#include "FuncA.h"
+#include "FuncA.h" 
 #include <cmath>
 
 double FuncA::calculate(double x) {
+	int n = 10; // Використовуйте n як аргумент у майбутньому 
 	double result = 0; 
-	for (int i = 1; i <= 3; ++i) { 
-		result += pow(-1, i - 1) * pow(x, i) / i;
- 	} 
-return result; 
-}
-
+	for (int i = 1; i <= n; ++i) { 
+		result += pow(-1, i - 1) * pow(x, i) / i; } 
+	return result; }
